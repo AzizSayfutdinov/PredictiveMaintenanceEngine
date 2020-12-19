05b69a1c25f2f033a3e86c7de759bbb92d210c7d
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-12-19 08:45:53 +0100

    Added images

---
+++ images/rf.py
@@ -0,0 +1,17 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Activation
+
+model = Sequential()
+
+model.add(LSTM(
+         input_shape=(timestamp, nb_features),
+         units=100,
+         return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(
+          units=50,
+          return_sequences=False))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1, activation='sigmoid'))
